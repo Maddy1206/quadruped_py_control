f9a06e1f0a22b18d6cc5529340e36e15c84b3a8c
Martin Untenberger <muntenberger@gmail.com>
2023-05-31 19:48:21 +0200

    Commiting files without submodule

---
+++ examples_py/py_test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+
+######
+#   
+#
+#
+######
+
+import sys
+import time
+import math
+
+sys.path.append('/home/ros/unitree_legged_sdk_py11/lib/python')
+
+import robot_interface as sdk
+
+if __name__ == '__main__':
+
+    HIGHLEVEL = 0x00
+    LOWLEVEL = 0xff
+
+    A1 = sdk.LeggedType.A1
+
+    BASIC = sdk.HighLevelType.Basic
+    SPORT = sdk.HighLevelType.Sport
+
+    udp = sdk.UDP(HIGHLEVEL, BASIC)
+
+    cmd = sdk.HighCmd()
+    state = sdk.HighState()
+    udp.InitCmdData(cmd)
+    motiontime = 0
+
+    cmd.forwardSpeed = 0
+    cmd.sideSpeed = 0
+    cmd.rotateSpeed = 0
+    cmd.bodyHeight = 0
+
+    cmd.mode = 0
+    cmd.roll = 0
+    cmd.pitch = 0
+    cmd.yaw = 0
+
+    while True:
+        time.sleep(0.01)
+        motiontime = motiontime + 1
+
+        udp.Recv()
+        udp.GetRecv(state)
+
+        print(motiontime)
+        # print(state.imu.temperature)
+
+        if(motiontime > 0 and motiontime < 1000):
+            cmd.mode = 2
+            cmd.forwardSpeed = 0.3
+            print("walk\n")
+
+        udp.SetSend(cmd)
+        udp.Send()
+
+        if(motiontime > 1000):
+            break
+
+
+
