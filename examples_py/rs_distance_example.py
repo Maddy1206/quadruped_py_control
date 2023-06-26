f4954905cd3734a9ad50096fd1787ba427dddcc1
Martin Untenberger <muntenberger@gmail.com>
2023-06-26 14:17:23 +0200

    added compiled realsense libs and usage with python code

---
+++ examples_py/rs_distance_example.py
@@ -0,0 +1,52 @@
+
+
+import sys 
+sys.path.append('/home/unitree/quadruped_py_control/lib/python/')
+import pyrealsense2 as rs
+import numpy as np
+import cv2
+
+pipeline = rs.pipeline()
+
+config = rs.config()
+config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+profile = pipeline.start(config)
+
+try:
+  while True:
+    frames = pipeline.wait_for_frames()
+    depth_frame = frames.get_depth_frame()
+    color_frame = frames.get_color_frame()
+
+    distance = depth_frame.get_distance(320, 240)
+    print(round(distance, 2))
+
+    depth_image = np.asanyarray(depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+
+    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+
+    depth_colormap_dim = depth_colormap.shape
+    color_colormap_dim = color_image.shape
+
+    # If depth and color resolutions are different, resize color image to match depth image for display
+    if depth_colormap_dim != color_colormap_dim:
+        resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+        images = np.hstack((resized_color_image, depth_colormap))
+    else:
+        images = np.hstack((color_image, depth_colormap))
+
+    cv2.rectangle(images,(320,240),(310,230),(0,0,255),-1)
+
+    # Show images
+    cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
+    cv2.imshow('RealSense', images)
+    cv2.waitKey(1)
+
+finally:
+
+    # Stop streaming
+    pipeline.stop()
+    
---
+++ examples_py/stay_in_range.py
@@ -0,0 +1,62 @@
+import sys, time, math
+import numpy as np
+import cv2
+
+sys.path.append('/home/unitree/quadruped_py_control/lib/python') # change the path to the respective folder
+import pyrealsense2 as rs           # Import the CPython Realsense library
+import robot_interface as sdk       # Import the CPython Interface library
+
+
+HIGHLEVEL = 0x00
+A1 = sdk.LeggedType.A1
+BASIC = sdk.HighLevelType.Basic
+SPORT = sdk.HighLevelType.Sport
+udp = sdk.UDP(HIGHLEVEL, BASIC)
+cmd = sdk.HighCmd()
+state = sdk.HighState()
+udp.InitCmdData(cmd)
+motiontime = 0
+cmd.forwardSpeed = 0
+cmd.mode = 0
+cmd.rotateSpeed = 0
+cmd.sideSpeed = 0.15 # nullify uneven calibration
+
+pipeline = rs.pipeline()
+config = rs.config()
+# config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+
+profile = pipeline.start(config)
+
+while True:
+  frames = pipeline.wait_for_frames()
+  depth_frame = frames.get_depth_frame()
+  # color_frame = frames.get_color_frame()
+
+  distance = depth_frame.get_distance(320, 240)
+  print(round(distance, 2))
+
+  udp.Recv()
+  udp.GetRecv(state)
+
+  if distance == 0:
+      continue
+
+  if distance < 1:
+    cmd.mode = 2
+    cmd.forwardSpeed = -0.3
+    print("walking backwards\n")
+
+  if distance > 1.3:
+    cmd.mode = 2
+    cmd.forwardSpeed = 0.3
+    print("walking forward\n")
+
+  if distance > 1 and distance < 1.3:
+    cmd.mode = 0
+    cmd.forwardSpeed = 0
+    print("waiting...")
+
+  udp.SetSend(cmd)
+  udp.Send()
